2acf20d74079d2b7b184653c408f6cc3610a1814
Thom Wiggers <thom@thomwiggers.nl>
2021-02-25 17:07:06 +0100

    tweak matomo

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,12 +1,15 @@
 /* stats.js */
-var _paq = window._paq || [];
+var _paq = window._paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(["setDoNotTrack", true]);
+_paq.push(['addDownloadExtensions', "bib"]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
+_paq.push(['setRequestMethod', 'POST']);
 (function() {
-  var u="//matomo.rded.nl/";
-  _paq.push(['setTrackerUrl', u+'matomo.php']);
+  var u="https://matomo.rded.nl/";
+  _paq.push(['setTrackerUrl', u+'m.track']);
   _paq.push(['setSiteId', '1']);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  g.type='text/javascript'; g.async=true; g.src=u+'/js/'; s.parentNode.insertBefore(g,s);
 })();
